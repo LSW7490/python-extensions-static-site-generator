740aafda86778fe6d79556536b5706992d33bd85
Shengwei LI <antoniovvtonyvv@gmail.com>
2022-06-08 09:05:55 +0200

    ok to use directly the method in imported modules

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,6 +6,6 @@ files = []
 def collect_files(source, site_parsers):
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in list(hooks.filter(valid, site_parsers)):
-            if parser.valid_file_ext(path.suffix()):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
                 files.append(path)
